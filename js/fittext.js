42508723458247df907e904f29b5ba015b4fbfb8
Tyler Davis <tylerdavis@lucidchart.com>
2017-10-12 07:51:06 -0600

    Added post about firefox, upgraded Hugo (by alot)

--- js/fittext.js
+++ js/fittext.js
@@ -1,19 +1,31 @@
-// fittext.js plugin
-          (function($){
-            $.fn.fitText = function(kompressor, options){
-              var compressor = kompressor || 1,
-                  settings = $.extend({
-                    'minFontSize' : Number.NEGATIVE_INFINITY,
-                    'maxFontSize' : Number.POSITIVE_INFINITY
-                  }, options);
-            
-              return this.each(function(){
-                var $this = $(this);
-                var resizer = function () {
-                  $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-                };
-                resizer();
-                $(window).on('resize orientationchange', resizer);
-              });
+(function ($) {
+    $.fn.fitText = function (kompressor, options) {
+        var compressor = kompressor || 1,
+            settings = $.extend({
+                'minFontSize': Number.NEGATIVE_INFINITY,
+                'maxFontSize': Number.POSITIVE_INFINITY
+            }, options);
+
+        return this.each(function () {
+            var $this = $(this);
+            var resizer = function () {
+                $this.css('font-size', Math.max(Math.min($this.width() / (compressor * 10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
             };
-          })( jQuery ); 
+            resizer();
+            $(window).on('resize orientationchange', resizer);
+        });
+    };
+    $('.read-more').click(function () {
+        var offscreen = $('.offscreen');
+        var body = $('.read-more + .collapse-read-more + .body');
+
+        body.html(offscreen.clone().html());
+
+        body.css({'max-height': offscreen.height() + 'px'})
+        $('.read-more').addClass('expand');
+    });
+    $('.collapse-read-more').click(function () {
+        $('.read-more + .collapse-read-more + .body').css({'max-height': '0'})
+        $('.read-more').removeClass('expand');
+    });
+})(jQuery);
